6711855cb850086d87a963a6a1edbe4d46f2732a
jack.yun <jack.yun@yelloexchange.com>
2017-01-15 20:27:32 +0900

    feat : upgrade module versions for RN 0.38, supress loginState Pertsistence due to complict

--- App/Navigation/NavigationRouter.js
+++ App/Navigation/NavigationRouter.js
@@ -1,7 +1,7 @@
 // @flow
 
 import React, { Component } from 'react'
-import { Scene, Router, ActionConst, Actions, Switch } from 'react-native-router-flux'
+import { Scene, Router, ActionConst, Actions } from 'react-native-router-flux'
 import Styles from './Styles/NavigationContainerStyle'
 import NavItems from './NavItems'
 import TabIcon from '../Components/TabIcon'
@@ -29,155 +29,147 @@ class NavigationRouter extends Component {
       <Router
         sceneStyle={{ backgroundColor: 'black' }}>
         <Scene
+          initial
+          key='Greeting'
+          hideNavBar
+          hideTabBar
+          component={GreetingScreen} />
+        <Scene
           key='root'
-          component={connect(mapStateToProps)(Switch)}
-          tabs
-          unmountScenes
-          selector={props => props.isLoggedIn ? 'main' : 'Greeting'} >
+          navigationBarStyle={Styles.navBar}
+          titleStyle={Styles.title}
+          leftButtonIconStyle={Styles.leftButton}
+          backButtonTextStyle={Styles.backButton}
+          rightButtonTextStyle={Styles.rightButton}>
           <Scene
-            key='Greeting'
-            hideNavBar
-            hideTabBar
-            component={GreetingScreen} />
-          <Scene
-            initial
-            key='main'
-            navigationBarStyle={Styles.navBar}
-            titleStyle={Styles.title}
-            leftButtonIconStyle={Styles.leftButton}
-            backButtonTextStyle={Styles.backButton}
-            rightButtonTextStyle={Styles.rightButton}>
+            key='tabBar'
+            tabs
+            tabBarStyle={{backgroundColor: '#000000', justifyContent: 'center', alignItems: 'center', alignSelf: 'center', height: 48.5}}>
             <Scene
-              key='tabBar'
-              tabs
-              tabBarStyle={{backgroundColor: '#000000', justifyContent: 'center', alignItems: 'center', alignSelf: 'center', height: 48.5}}>
+              key='homeTab'
+              icon={TabIcon}
+              selectedTabIcon='home'
+              tabIcon='home'
+              onPress={() => {
+                this.props.registerScreen('homeTab')
+                Actions.homeTab()
+              }}
+              navigationBarStyle={Styles.navBar}
+              leftButtonIconStyle={Styles.leftButton}
+              titleStyle={{color: 'white', fontSize: 20, fontWeight: 'bold'}}>
               <Scene
-                key='homeTab'
                 initial
-                icon={TabIcon}
-                selectedTabIcon='home'
-                tabIcon='home'
-                onPress={() => {
-                  this.props.registerScreen('homeTab')
-                  Actions.homeTab()
-                }}
-                navigationBarStyle={Styles.navBar}
-                leftButtonIconStyle={Styles.leftButton}
-                titleStyle={{color: 'white', fontSize: 20, fontWeight: 'bold'}}>
-                <Scene
-                  initial
-                  key='feedScreen'
-                  panHandlers={null}
-                  component={FeedScreen}
-                  renderTitle={NavItems.episodeLogo} />
-                <Scene
-                  key='feedTouserProfileScreen'
-                  component={UserProfileScreen}
-                  title='프로필' />
-                <Scene
-                  key='commentScreen'
-                  component={CommentScreen}
-                  title='댓글'
-                  renderLeftButton={NavItems.chevronButton} />
-                <Scene
-                  key='followScreen'
-                  component={FollowScreen}
-                  title='follow'
-                  renderLeftButton={NavItems.chevronButton} />
-                <Scene
-                  key='followerScreen'
-                  component={FollowerScreen}
-                  title='follower'
-                  renderLeftButton={NavItems.chevronButton} />
-              </Scene>
+                key='feedScreen'
+                panHandlers={null}
+                component={FeedScreen}
+                renderTitle={NavItems.episodeLogo} />
               <Scene
-                key='alarmTab'
-                icon={TabIcon}
-                selectedTabIcon='bell-o'
-                tabIcon='bell'
-                onPress={() => {
-                  this.props.registerScreen('alarmTab')
-                  Actions.alarmTab()
-                }}
-                navigationBarStyle={Styles.navBar}
-                leftButtonIconStyle={Styles.leftButton}
-                titleStyle={{color: 'white', fontSize: 20, fontWeight: 'bold'}}>
-                <Scene
-                  key='notiScreen'
-                  panHandlers={null}
-                  component={NotiScreen}
-                  title='내소식' />
-                <Scene
-                  key='singleEpisodeScreen'
-                  component={SingleEpisodeScreen}
-                  direction='vertical'
-                  title='에피소드' />
-                <Scene
-                  key='notiTouserProfileScreen'
-                  component={UserProfileScreen}
-                  title='프로필' />
-              </Scene>
+                key='feedTouserProfileScreen'
+                component={UserProfileScreen}
+                title='프로필' />
               <Scene
-                key='cameraTab'
-                icon={TabIcon}
-                selectedTabIcon='camera'
-                tabIcon='camera'
-                navigationBarStyle={Styles.navBar}
-                onPress={() => {
-                  Actions.cameraScreen({isOpen: true, type: ActionConst.REFRESH})
-                }}
-                titleStyle={{color: 'white', fontSize: 17}}>
-                <Scene
-                  key='cameraScreen'
-                  panHandlers={null}
-                  component={CameraScreen}
-                  renderTitle={NavItems.episodeLogo}
-                  hideNavBar
-                  hideTabBar />
-              </Scene>
+                key='commentScreen'
+                component={CommentScreen}
+                title='댓글'
+                renderLeftButton={NavItems.chevronButton} />
               <Scene
-                key='searchTab'
-                icon={TabIcon}
-                selectedTabIcon='search'
-                tabIcon='search'
-                onPress={() => {
-                  this.props.registerScreen('searchTab')
-                  Actions.searchTab()
-                }}
-                leftButtonIconStyle={Styles.leftButton}
-                navigationBarStyle={Styles.navBar}
-                titleStyle={{color: 'white', fontSize: 20, fontWeight: 'bold'}}>
-                <Scene
-                  key='exploreScreen'
-                  panHandlers={null}
-                  component={ExploreScreen}
-                  title='우연한 발견' />
-                <Scene
-                  key='searchTouserProfileScreen'
-                  component={UserProfileScreen}
-                  title='프로필' />
-                <Scene
-                  key='searchTosingleEpisodeScreen'
-                  component={SingleEpisodeScreen}
-                  title='에피소드' />
-              </Scene>
+                key='followScreen'
+                component={FollowScreen}
+                title='follow'
+                renderLeftButton={NavItems.chevronButton} />
               <Scene
-                key='profileTab'
-                icon={TabIcon}
-                selectedTabIcon='user'
-                tabIcon='user'
-                onPress={() => {
-                  this.props.registerScreen('profileTab')
-                  Actions.profileTab()
-                }}
-                navigationBarStyle={Styles.navBar}
-                titleStyle={{color: 'white', fontSize: 20, fontWeight: 'bold'}}>
-                <Scene
-                  key='profileScreen'
-                  panHandlers={null}
-                  component={ProfileScreen}
-                  title='내 프로필' />
-              </Scene>
+                key='followerScreen'
+                component={FollowerScreen}
+                title='follower'
+                renderLeftButton={NavItems.chevronButton} />
+            </Scene>
+            <Scene
+              key='alarmTab'
+              icon={TabIcon}
+              selectedTabIcon='bell-o'
+              tabIcon='bell'
+              onPress={() => {
+                this.props.registerScreen('alarmTab')
+                Actions.alarmTab()
+              }}
+              navigationBarStyle={Styles.navBar}
+              leftButtonIconStyle={Styles.leftButton}
+              titleStyle={{color: 'white', fontSize: 20, fontWeight: 'bold'}}>
+              <Scene
+                key='notiScreen'
+                panHandlers={null}
+                component={NotiScreen}
+                title='내소식' />
+              <Scene
+                key='singleEpisodeScreen'
+                component={SingleEpisodeScreen}
+                direction='vertical'
+                title='에피소드' />
+              <Scene
+                key='notiTouserProfileScreen'
+                component={UserProfileScreen}
+                title='프로필' />
+            </Scene>
+            <Scene
+              key='cameraTab'
+              icon={TabIcon}
+              selectedTabIcon='camera'
+              tabIcon='camera'
+              navigationBarStyle={Styles.navBar}
+              onPress={() => {
+                Actions.cameraScreen({isOpen: true, type: ActionConst.REFRESH})
+              }}
+              titleStyle={{color: 'white', fontSize: 17}}>
+              <Scene
+                key='cameraScreen'
+                panHandlers={null}
+                component={CameraScreen}
+                renderTitle={NavItems.episodeLogo}
+                hideNavBar
+                hideTabBar />
+            </Scene>
+            <Scene
+              key='searchTab'
+              icon={TabIcon}
+              selectedTabIcon='search'
+              tabIcon='search'
+              onPress={() => {
+                this.props.registerScreen('searchTab')
+                Actions.searchTab()
+              }}
+              leftButtonIconStyle={Styles.leftButton}
+              navigationBarStyle={Styles.navBar}
+              titleStyle={{color: 'white', fontSize: 20, fontWeight: 'bold'}}>
+              <Scene
+                key='exploreScreen'
+                panHandlers={null}
+                component={ExploreScreen}
+                title='우연한 발견' />
+              <Scene
+                key='searchTouserProfileScreen'
+                component={UserProfileScreen}
+                title='프로필' />
+              <Scene
+                key='searchTosingleEpisodeScreen'
+                component={SingleEpisodeScreen}
+                title='에피소드' />
+            </Scene>
+            <Scene
+              key='profileTab'
+              icon={TabIcon}
+              selectedTabIcon='user'
+              tabIcon='user'
+              onPress={() => {
+                this.props.registerScreen('profileTab')
+                Actions.profileTab()
+              }}
+              navigationBarStyle={Styles.navBar}
+              titleStyle={{color: 'white', fontSize: 20, fontWeight: 'bold'}}>
+              <Scene
+                key='profileScreen'
+                panHandlers={null}
+                component={ProfileScreen}
+                title='내 프로필' />
             </Scene>
           </Scene>
         </Scene>
@@ -188,7 +180,6 @@ class NavigationRouter extends Component {
 
 const mapStateToProps = (state) => {
   return {
-    isLoggedIn: state.token.id !== null && state.token.token !== null,
     beforeScreen: state.screen.beforeScreen
   }
 }
